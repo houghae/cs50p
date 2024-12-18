e35ddb4265102ac3600a654157e11350bc504a55
Arthur Hough <hough1ae@gmail.com>
2024-12-18 12:57:09 -0700

    add get_data and get_alpaca_data functions

--- project/projectV3.py
+++ project/projectV3.py
@@ -1,40 +1,76 @@
 # Dependencies.
+# Use matplotlib for charts, mplfinance is a specific module for charting stocks.
+# Use Alpaca for stock data. Arguably more robust than yfinance.
+# Use pandas for data handling and I/O. pandas is used heavily for data analysis.
+# Use os to create dir
 import matplotlib.pyplot as plt
-import csv
-import os
 import mplfinance as mpf
 from alpaca.data import StockHistoricalDataClient
-import pandas
+from alpaca.data.requests import StockBarsRequest
+from alpaca.data.timeframe import TimeFrame, TimeFrameUnit
+import pandas as pd
+import os
+from datetime import datetime, timedelta
+from zoneinfo import ZoneInfo
+
 
 # Alpaca trading keys and client.
-# Paper trading key only. 
-API_key = "PKJUWQ85O4FFNJPN99RC"
-API_secret = "dicyifzWYOsz6K76AXUoa8ogI57sjPCAwrhyarDZ"
+# Paper trading key only.
+# Capitalize vars that are constants, like API keys and dir paths. 
+# Blur or replace keys before filming.
+API_KEY = "placeholder"
+API_SECRET = "placeholder"
 
-alpaca_client = StockHistoricalDataClient(API_key, API_secret)
+alpaca_client = StockHistoricalDataClient(API_KEY, API_SECRET)
 
-# File paths for CSV storage
-
+# Create file path for CSV storage in case there isn't yet a dir. 
+# This enables portability and makes the code more robust if I share it.
+HIST_DATA_DIR = "historical_data"
+os.makedirs(HIST_DATA_DIR, exist_ok=True)
 
 # Classes for indicators
+
 
 
 # Class for matplotlib visualization
 # Plot multiple timeframe charts each with their own indicators.
 
 
-# Get CSV data function. 
+
 # If CSV data exists already, return it. Else run a function to get it from Alpaca.
+def get_data(ticker, timeframe):
+    # create csv var named ticker_timetrame.csv
+    csv_file = os.path.join(HIST_DATA_DIR, f"{ticker}_{timeframe}.csv")
+    # search hist data dir for that file. If it exists, return it. Else run the get alpaca data function.
+    if os.path.exists(csv_file):
+        return pd.read_csv(csv_file) # Prob need to do this later, pd.read_csv(filepath, index_col="timestamp", parse_dates=True)
+    else:
+        alpaca_data = get_alpaca_data(ticker, timeframe)
+        alpaca_data.to_csv(csv_file)
+        return alpaca_data
 
 
 # Get Alpaca data function. 
-# Get weekly, daily, 4hr, and 1hr data. Max timeframe, candlestick or OHLC data.
+def get_alpaca_data(ticker, timeframe):
+    # Get weekly, daily, 4hr, and 1hr data. Max timeframe, candlestick or OHLC data.
+    now = datetime.now(ZoneInfo("America/New_York"))
+    tf_dict = {
+        "1-Hour": [TimeFrameUnit.Hour, 1],
+        "4-Hour": [TimeFrameUnit.Hour, 4],
+        "Daily": [TimeFrameUnit.Day, 1],
+        "Weekly": [TimeFrameUnit.Week, 1],
+    }
 
+    request = StockBarsRequest(
+        symbol_or_symbols = [ticker],
+        # TFU can be Hour, Day, Week, Month. Use amount = 4 for 4hr timeframe, 1 for day and week.
+        timeframe=TimeFrame(amount=tf_dict[timeframe][1], unit=tf_dict[timeframe][0]), # specify timeframe
+        start=now - timedelta(days = 1827), # specify start datetime, default=the beginning of the current day.
+        # end_date=None, # specify end datetime, default=now
+        limit=10, # specify number of data points
+    )
+    return alpaca_client.get_stock_bars(request).df
 
-# Save CSV data function
-
-
-# Read CSV data function
 
 
 def main():
